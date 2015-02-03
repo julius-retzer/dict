8ebb44c9be309420d57e07027d2bfeec11711bb5
Jules Retzer <jul.retzer@gmail.com>
2015-02-03 21:41:46 +0100

    angular is working

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,11 @@
 'use strict';
 
 /* App Module */
+
+(function() {
+  var dictApp = angular.module('dictApp', []);
+
+  dictApp.controller('LanguageController', ['$scope', function($scope){
+    $scope.test = 'Hello';
+  }]);
+})();
