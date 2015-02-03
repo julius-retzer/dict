65df406fb0b3895d7c2e586bf2a1babaaa2be068
Jules Retzer <jul.retzer@gmail.com>
2015-02-03 23:43:03 +0100

    retrieves languages and passes the first unit test

--- app/js/app.js
+++ app/js/app.js
@@ -4,8 +4,12 @@
 
 (function() {
   var dictApp = angular.module('dictApp', []);
-
-  dictApp.controller('LanguageController', ['$scope', function($scope){
-    $scope.test = 'Hello';
+  
+  dictApp.controller('LanguageCtrl', ['$scope', '$http', function($scope, $http){
+      $http.get('http://private-9b43f-translateservice.apiary-mock.com/languages').success(function(data) {
+        $scope.languages = data;
+    });
   }]);
+  
+    
 })();
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -2,10 +2,32 @@
 
 /* jasmine specs for controllers go here */
 
-describe('controllers', function() {
+describe('LanguageCtrl', function () {
+    
+    var scope, ctrl, $httpBackend;
+    
+    
+    beforeEach(module('dictApp'));
+    
+    beforeEach(inject(function (_$httpBackend_, $rootScope, $controller) {
+        $httpBackend = _$httpBackend_;
+        
+        $httpBackend.expectGET('http://private-9b43f-translateservice.apiary-mock.com/languages')
+        .respond(["english", "czech"]);
 
-  it("should do something", function() {
+        
+        scope = $rootScope.$new();
+        
+        ctrl = $controller('LanguageCtrl', {$scope: scope});
+    }));
+    
+    
+    it("should retrieve language list", inject(function () {
+        expect(scope.languages).toBeUndefined();
+        $httpBackend.flush();
 
-  });
+        expect(scope.languages).toEqual(["english", "czech"]);
+    }));
+    
 
 });
