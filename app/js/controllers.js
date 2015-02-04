5549b973eae39158facfa09fc985cfe0f40b64cc
Jules Retzer <jul.retzer@gmail.com>
2015-02-04 23:15:39 +0100

    deleted unnecesary setLanguage

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,21 +6,17 @@
   var dictControllers = angular.module('dictControllers', ['dictServices']);
 
   dictControllers.controller('LanguageCtrl', ['$scope', 'translationService', function($scope, translationService){
-    
-    $scope.activeLanguage = 'english'
-    
+
+    $scope.activeLanguage = 'english';
+
     translationService.getLanguages().success(function(response){
       $scope.languages = response;
-    })
+    });
 
-    $scope.getWords = translationService.getWords($scope.activeLanguage).success(function(response){
-      $scope.words = response;
-    })
-
-
-
-    $scope.setLanguage = function(language) {
-      $scope.activeLanguage = language;
+    $scope.getWords = function(language) {
+      translationService.getWords(language).success(function(response){
+        $scope.words = response;
+      });
     };
 
 
