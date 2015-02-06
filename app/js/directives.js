46515c45e7ba3374f9e0c2625fe73d835c94ff32
Jules Retzer <jul.retzer@gmail.com>
2015-02-06 22:48:41 +0100

    new style and input area resizing

--- app/js/directives.js
+++ app/js/directives.js
@@ -2,7 +2,7 @@
 
 /* Directives */
 
-var dictApp = angular.module('dictDirectives',[]);
+var dictApp = angular.module('dictDirectives', []);
 
 
 
@@ -14,32 +14,38 @@ dictApp.directive('editable', function () {
         },
         templateUrl: 'partials/editable.html',
         link: function (scope, element, attrs) {
-            
+
             var inputElement = angular.element(element.children()[1]);
-            
+            var spanElement = element.children()[0]
             element.addClass('editable');
 
             scope.isEditing = false;
-            
+
             scope.edit = function () {
                 scope.isEditing = true;
 
                 // We control display through a class on the directive itself. See the CSS.
                 element.addClass('active');
 
-                
+
                 inputElement[0].focus();
             };
-            
+
             // When we leave the input, we're done editing.
             inputElement.bind('blur keyup', function (e) {
-                if (e.type === 'blur' || e.keyCode === 13){
-                   scope.isEditing = false;
+                if (e.type === 'blur' || e.keyCode === 13) {
+                    scope.isEditing = false;
                     element.removeClass('active');
                     scope.$apply();
                 }
             });
+
+            function resizeInput() {
+                $(this).attr('size', $(this).val().length);
+            }
+
+            inputElement.focus(resizeInput).keyup(resizeInput).each(resizeInput);
+
         }
     };
 });
-
